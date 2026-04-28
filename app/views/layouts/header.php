b9410a09eff2ddcae6365c641785965b919d899e
Nguyen Khac Thai Khang <khang2122004@gmail.com>
2026-04-28 23:30:50 +0700

    add SEO file

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -13,6 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?php echo htmlspecialchars($resolvedPageDescription, ENT_QUOTES, 'UTF-8'); ?>">
     <title><?php echo htmlspecialchars($fullTitle, ENT_QUOTES, 'UTF-8'); ?></title>
+    <link rel="sitemap" type="application/xml" title="Sitemap" href="sitemap.xml">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/responsive.css">
 </head>
