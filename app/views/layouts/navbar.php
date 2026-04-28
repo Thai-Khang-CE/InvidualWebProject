8f9e87fcb41c5320888eeac93ad5506c153b3ff6
Nguyen Khac Thai Khang <khang2122004@gmail.com>
2026-04-28 22:44:15 +0700

    logout

--- app/views/layouts/navbar.php
+++ app/views/layouts/navbar.php
@@ -3,13 +3,22 @@
         <a class="site-logo" href="index.php?page=home">Seasonal Wardrobe</a>
 
         <nav class="site-nav" aria-label="Primary navigation">
+            <?php
+            $isLoggedIn = isset($_SESSION['user_id'], $_SESSION['user_name']);
+            $userName = $isLoggedIn ? htmlspecialchars((string) $_SESSION['user_name'], ENT_QUOTES, 'UTF-8') : '';
+            ?>
             <ul class="nav-list">
                 <li><a class="nav-link" href="index.php?page=home">Home</a></li>
                 <li><a class="nav-link" href="index.php?page=products">Products</a></li>
                 <li><a class="nav-link" href="index.php?page=search">Search</a></li>
                 <li><a class="nav-link" href="index.php?page=contact">Contact</a></li>
-                <li><a class="nav-link" href="index.php?page=login">Login</a></li>
-                <li><a class="nav-link" href="index.php?page=register">Register</a></li>
+                <?php if ($isLoggedIn) : ?>
+                    <li><span class="nav-greeting">Hello, <?php echo $userName; ?></span></li>
+                    <li><a class="nav-link" href="index.php?page=logout">Logout</a></li>
+                <?php else : ?>
+                    <li><a class="nav-link" href="index.php?page=login">Login</a></li>
+                    <li><a class="nav-link" href="index.php?page=register">Register</a></li>
+                <?php endif; ?>
             </ul>
         </nav>
     </div>
--- public/index.php
+++ public/index.php
@@ -20,6 +20,27 @@ $allowedPages = [
 
 $page = isset($_GET['page']) && is_string($_GET['page']) ? $_GET['page'] : 'home';
 
+if ($page === 'logout') {
+    $_SESSION = [];
+
+    if (ini_get('session.use_cookies')) {
+        $params = session_get_cookie_params();
+        setcookie(
+            session_name(),
+            '',
+            time() - 42000,
+            $params['path'],
+            $params['domain'],
+            $params['secure'],
+            $params['httponly']
+        );
+    }
+
+    session_destroy();
+    header('Location: index.php?page=home');
+    exit;
+}
+
 if ($page === 'api_search') {
     header('Content-Type: application/json; charset=utf-8');
 
